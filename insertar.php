b019ec7602086a56de10489ae98d97169b869793
gustavo <gustavodevaler@gmail.com>
2019-01-25 09:53:56 -0300

    fix

--- insertar.php
+++ insertar.php
@@ -10,11 +10,11 @@ if(!$_SESSION){
 <div class="container-insert">
 <form method="POST" action="insert.php">
 	Descripcion:<br>
-	<input type="text" name="descripcion" required=""><br>
+	<input type="text" name="descripcion" required ><br>
 	Numero de articulo:<br>
-	<input type="text" name="nro_art"><br>
+	<input type="text" name="nro_art" pattern="[0-9]+"><br>
 	Talle:<br>
-	<input type="text" name="talle" required=""><br>
+	<input type="text" name="talle" required="" pattern="[0-9]+"><br>
 	Color:<br>
 	<input type="text" name="color"><br>
 	Fabricante:<br>
@@ -22,9 +22,9 @@ if(!$_SESSION){
 	Jugador:<br>
 	<input type="text" name="jugador"><br>
 	Temporada:<br>
-	<input type="text" name="temporada"><br>
+	<input type="text" name="temporada" pattern="[0-9]+"><br>
 	Cantidad:<br>
-	<input type="text" name="cantidad"><br>
+	<input type="text" name="cantidad" pattern="[0-9]+"><br>
 	Precio:<br>
 	<input type="text" name="precio" required=""><br>
 	Precio mayorista:<br>
