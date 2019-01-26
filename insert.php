69597025d1e2f803637479e22140d90dec1d145a
gustavo <gustavodevaler@gmail.com>
2019-01-26 10:01:46 -0300

    imagenes

--- insert.php
+++ insert.php
@@ -10,10 +10,17 @@ $temporada=$_POST['temporada'];
 $cantidad=$_POST['cantidad'];
 $precio=$_POST['precio'];
 $precio_mayor=$_POST['precio_mayor'];
-$nombre_foto =$_FILES['imagen']['name'];
+$nombre_foto =$_FILES['foto']['name'];
 
 
-$sql2="INSERT INTO productos (descripcion,nro_art,talle,color,fabricante,jugador,temporada,cantidad,precio,precio_mayor)VALUES ('$descripcion','$nro_art','$talle','$color','$fabricante','$jugador','$temporada','$cantidad','$precio','$precio_mayor')";
+
+$sql2="INSERT INTO productos (descripcion,nro_art,talle,color,fabricante,jugador,temporada,cantidad,precio,precio_mayor,nombre_foto)VALUES ('$descripcion','$nro_art','$talle','$color','$fabricante','$jugador','$temporada','$cantidad','$precio','$precio_mayor','$nombre_foto')";
+if (file_exists("imagenes/".$_FILES["foto"]["name"])){
+	echo $_FILES["foto"]["name"]. "ya existe.";
+}
+else{
+	move_uploaded_file($_FILES["foto"]["tmp_name"], "imagenes/".$_FILES["foto"]["name"]);
+}
 
 if(mysqli_query($conn,$sql2)){
 	echo "guardado con exito";
